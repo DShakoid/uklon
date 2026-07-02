2fa91870ba292d735e7db6f79831a3d064daf456
DShakoid <dimanshapko@gmail.com>
2026-07-02 07:51:17 +0300

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -34,7 +34,7 @@ export default function Layout({ children, pageTitle, activePage, setActivePage 
             
             <div className="user-profile user-profile2">
               <div className="avatar avatar2">
-<svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
 <style type="text/css">
 	.st0{fill:#FFFF55;}
