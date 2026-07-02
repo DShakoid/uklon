e42441581a031e3ccffd7e8e788cdc394108ae7e
DShakoid <dimanshapko@gmail.com>
2026-07-02 03:06:16 +0300

    Create MainPage.jsx

---
+++ src/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '40px', color: '#8a8d90' }}>
+      <h2>Карта и Форма заказа</h2>
+      <p style={{ marginTop: '10px' }}>Здесь мы соберём форму такси, выбор тарифов и карту.</p>
+    </div>
+  );
+}
