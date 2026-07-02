f58072fbb787a0fe05755c9d3366635f7e983c6e
DShakoid <dimanshapko@gmail.com>
2026-07-02 07:00:55 +0300

    Update MainPage.jsx

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -4,10 +4,10 @@ import './MainPage.css';
 export default function MainPage() {
   const services = [
     { id: 'delivery', name: 'Доставка', icon: '📦', isBeta: false },
-    { id: 'travel', name: 'Путешествия', icon: '🚌', isBeta: true },
-    { id: 'flowers', name: 'Цветы', icon: '💐', isBeta: true },
-    { id: 'assist', name: 'Автоподмога', icon: '🛠️', isBeta: false },
-    { id: 'intercity', name: 'Межгород', icon: '🛣️', isBeta: false },
+    { id: 'travel', name: 'Подорожі', icon: '🚌', isBeta: true },
+    { id: 'flowers', name: 'Квіти', icon: '💐', isBeta: true },
+    { id: 'assist', name: 'Автопідмога', icon: '🛠️', isBeta: false },
+    { id: 'intercity', name: 'Міжміст', icon: '🛣️', isBeta: false },
     { id: 'donate', name: 'Донат', icon: '🛸', isBeta: false },
   ];
 
@@ -22,7 +22,7 @@ export default function MainPage() {
         <div className="map-pin double-ring" style={{ top: '50%', left: '48%' }}>🔄</div>
         
         {/* Кнопка Моя локация справа */}
-        <button className="geo-location-btn">🎯</button>
+        <button className="geo-location-btn"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M440-42v-80q-125-14-214.5-103.5T122-440H42v-80h80q14-125 103.5-214.5T440-838v-80h80v80q125 14 214.5 103.5T838-520h80v80h-80q-14 125-103.5 214.5T520-122v80h-80Zm238-240q82-82 82-198t-82-198q-82-82-198-82t-198 82q-82 82-82 198t82 198q82 82 198 82t198-82Zm-311-85q-47-47-47-113t47-113q47-47 113-47t113 47q47 47 47 113t-47 113q-47 47-113 47t-113-47Zm169.5-56.5Q560-447 560-480t-23.5-56.5Q513-560 480-560t-56.5 23.5Q400-513 400-480t23.5 56.5Q447-400 480-400t56.5-23.5ZM480-480Z"/></svg></button>
       </div>
 
       {/* НИЖНЯЯ ПАНЕЛЬ ЗАКАЗА */}
